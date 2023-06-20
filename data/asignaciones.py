c1b667a7bae31d34e22f9a26dbdc8782cce570c1
Bony2002 <valenfbi@gmail.com>
2023-06-20 16:24:31 -0300

    canciones

--- data/asignaciones.py
+++ data/asignaciones.py
@@ -8,25 +8,25 @@ playlist=data['canciones']
 
 for canciones in playlist:
     if canciones["valence"]<=0.3 and canciones["energy"]<=0.3:
-        canciones["blob"] = "11.svg"
+        canciones["blob"] = "medias/figuras/11.svg"
     if canciones["valence"]<=0.3 and 0.3<canciones["energy"]<=0.6:
-        canciones["blob"]="12.svg"
+        canciones["blob"]="medias/figuras/12.svg"
     if canciones["valence"]<=0.3 and 0.6<canciones["energy"]<=1:
-        canciones["blob"]="13.svg"
+        canciones["blob"]="medias/figuras/13.svg"
     
     if 0.3<canciones["valence"]<=0.6 and canciones["energy"]<=0.3:
-        canciones["blob"]="21.svg"
+        canciones["blob"]="medias/figuras/21.svg"
     if 0.3<canciones["valence"]<=0.6 and 0.3<canciones["energy"]<=0.6:
-        canciones["blob"]="22.svg"
+        canciones["blob"]="medias/figuras/22.svg"
     if 0.3<canciones["valence"]<=0.6 and 0.6<canciones["energy"]<=1:
-        canciones["blob"]="23.svg"
+        canciones["blob"]="medias/figuras/23.svg"
 
     if 0.6<canciones["valence"]<=1 and canciones["energy"]<=0.3:
-        canciones["blob"]="31.svg"
+        canciones["blob"]="medias/figuras/31.svg"
     if 0.6<canciones["valence"]<=1 and 0.3<canciones["energy"]<=0.6:
-        canciones["blob"]="32.svg"
+        canciones["blob"]="medias/figuras/32.svg"
     if 0.6<canciones["valence"]<=1 and 0.6<canciones["energy"]<=1:
-        canciones["blob"]="33.svg"
+        canciones["blob"]="medias/figuras/33.svg"
 
     
 
